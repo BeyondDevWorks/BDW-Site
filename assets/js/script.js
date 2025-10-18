b24823352358564dedec78de7e3ed7bf7e466104
Tobi-Technik <TobiasPolzer77@gmail.com>
2025-10-18 20:58:27 +0200

    New Release of Beyond Music 0.1.9

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,7 @@ if(copyBtn){
 
 
 const appImages = {
-  app1: ['assets/img/beyondmusic_show2.png', 'assets/img/beyondmusic_show.png', 'assets/img/beyondmusic_show3.png', 'assets/img/beyondmusic_show4.png',],
+  app1: ['assets/img/beyondmusic_show2_1.png', 'assets/img/beyondmusic_show_1.png', 'assets/img/beyondmusic_show3_1.png', 'assets/img/beyondmusic_show4_1.png',],
   app2: ['app2-1-gross.jpg', 'app2-2-gross.jpg']
 };
 
